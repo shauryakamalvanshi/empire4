75f427bb850c4ac85c4eeefe4a8b9d5d7cf2460b
shaurya kamalvanshi <kamalvanshishaurya@gmail.com>
2023-11-24 23:05:04 +0530

    6 commit

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -1,21 +1,22 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-export async function GET(){
+export async function GET() {
     try {
-        const response=NextResponse.json(
-            {
-                message:"Logout successful",
-                success:true,
-            }
-        )
-        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0), domain: "empiretechnology.vercel.app", path: "/" });
+        const response = NextResponse.json({
+            message: "Logout successful",
+            success: true,
+        });
 
-        response.cookies.set("token","",{httpOnly:true,expires:new Date(0)})
+        response.cookies.set("token", "", {
+            httpOnly: true,
+            expires: new Date(0),
+            domain: "empiretechnology.vercel.app", // Change this to your domain
+            path: "/", // Change this to your path
+            secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS in production
+        });
 
-      return response;
-      
-        
+        return response;
     } catch (error) {
-        return NextResponse.json({error:error.message},{status:500})
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
