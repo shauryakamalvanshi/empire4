772777eb1daffb9c042b3fcd35fb59fd9598413c
shaurya kamalvanshi <kamalvanshishaurya@gmail.com>
2023-11-24 21:00:36 +0530

    4 commit

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -8,7 +8,7 @@ export async function GET(){
                 success:true,
             }
         )
-        response.cookies.set("token","",{httpOnly:true,expires:new Date(0)})
+        response.cookies.set("token","",{httpOnly:false,expires:new Date(0)})
       return response;
       
         
